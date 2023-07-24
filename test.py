c2a452e17bbdbce324154deb39502931726c48ee
nicklagat <130362908+nicklagat@users.noreply.github.com>
2023-07-25 00:49:16 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
+        print("The tests are running")
 
 
 if __name__ == '__main__':
