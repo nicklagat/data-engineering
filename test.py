d9a4d081c5406feb2329f95058a3b4f67ac43752
nicklagat <nicksonlagatk@gmail.com>
2023-07-25 00:52:09 +0300

    added a print statement to the test class

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
+        print(1+2)
+        print("This is another print statement")
 
 
 if __name__ == '__main__':
